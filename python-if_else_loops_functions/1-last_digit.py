aeb5d8c9b191d0c753221ca1f85447baaa9fa012
a0dfc11ba139 <e.adewusi@alustudent.com>
2023-07-11 00:11:57 -0700

    Create: python script that assigns

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -4,6 +4,8 @@ import random
 number = random.randint(-10000, 10000)
 
 last_digit = abs(number) % 10
+if number < 0:
+    last_digit *= -1
 
 print("Last digit of", number, "is", last_digit, end=" ")
 
