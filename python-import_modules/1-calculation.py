e413b52dd861ef68e52b196c957d948f846eafce
a0dfc11ba139 <e.adewusi@alustudent.com>
2023-07-11 01:55:05 -0700

    Write: python script that imports and calculates

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+a = 10
+b = 5
+
+from calculator_1 import add, sub, mul, div
+
+print("{} + {} = {}".format(a, b, add(a, b)))
+print("{} - {} = {}".format(a, b, sub(a, b)))
+print("{} * {} = {}".format(a, b, mul(a, b)))
+print("{} / {} = {}".format(a, b, div(a, b)))
