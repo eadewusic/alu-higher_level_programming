8d2271ff1a3b34bee4b49d10a48e1a5096831064
a0dfc11ba139 <e.adewusi@alustudent.com>
2023-07-11 00:57:50 -0700

    Create: python script complete

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-uppercase = __import__('8-uppercase').uppercase
 
-uppercase("best")
-uppercase("Best School 98 Battery street")
+def uppercase(str):
+    new_string = ""
+    for char in str:
+        if (ord(char) in range(97, 123)):
+            new_string += chr(ord(char) - 32)
+        else:
+            new_string += char
+    print("{new_string}".format(new_string=new_string))
