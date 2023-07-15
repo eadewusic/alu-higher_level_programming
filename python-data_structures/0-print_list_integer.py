6f000feac314d1e1026ce2f0a174e92061eb41f9
a0dfc11ba139 <e.adewusi@alustudent.com>
2023-07-15 04:41:19 -0700

    Write: function that prints integers of a list

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     for num in my_list:
-        print("{}".format(num))
+        print("{:d}".format(num))
 
 my_list = [1, 2, 3, 4, 5]
 print_list_integer(my_list)
