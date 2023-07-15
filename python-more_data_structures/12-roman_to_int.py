0fa02d51b693e9deb2bb8c134c2009c216a35f59
a0dfc11ba139 <e.adewusi@alustudent.com>
2023-07-15 15:38:40 -0700

    Write: function tat converts roman numerals to integers

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+
+    roman_dict = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50,
+        'C': 100, 'D': 500, 'M': 1000
+    }
+
+    result = 0
+    prev_value = 0
+
+    for char in roman_string[::-1]:
+        value = roman_dict[char]
+        if value >= prev_value:
+            result += value
+        else:
+            result -= value
+        prev_value = value
+
+    return result
