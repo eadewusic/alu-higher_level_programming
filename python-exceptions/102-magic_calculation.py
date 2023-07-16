987d8c04581a62d0751b6b86b72961304192423d
a0dfc11ba139 <e.adewusi@alustudent.com>
2023-07-16 09:11:21 -0700

    Write: python function that does exactly as python bytecode

---
+++ python-exceptions/102-magic_calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    result = 0
+
+    for i in range(1, 4):
+        try:
+            if i > a:
+                raise Exception('Too far')
+            result += (a ** b) / i
+        except:
+            result = b + a
+            break
+
+    return result
