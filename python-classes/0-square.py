c0e5171ef4ce530cd1cd5fc52167501a149585b7
a0dfc11ba139 <e.adewusi@alustudent.com>
2023-07-28 00:04:34 -0700

    empty class square defining a square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""This is a module or python file that has a class called Square and this class Square performs an action."""
 
 class Square:
     """Empty class representing a square."""
