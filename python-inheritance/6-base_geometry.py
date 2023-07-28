3ee9357a0fa498c23113c5a8e3932543e6d040df
a0dfc11ba139 <e.adewusi@alustudent.com>
2023-07-28 14:56:32 -0700

    6. Improve Geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''Creates an empty class called BaseGeometry
+'''
+
+
+class BaseGeometry:
+    '''Class BaseGeometry
+    '''
+    def area(self):
+        '''Calculates area
+        '''
+        raise Exception("area() is not implemented")
