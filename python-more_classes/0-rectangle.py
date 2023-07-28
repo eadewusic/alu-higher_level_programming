3d3e8596547235670af5ac4f1c680261de28ed70
a0dfc11ba139 <e.adewusi@alustudent.com>
2023-07-28 01:12:30 -0700

    Write an empty class Rectangle that defines a rectangle

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This is a python script
+that contains class Rectangle.
+"""
+
+
+class Rectangle():
+    """This is an empty class
+    rectangle that has not attributes
+    or methods.
+    """
+    pass
---
+++ python-more_classes/README.md
@@ -0,0 +1,1 @@
+This directory contains different python scripts of which the first says to write an empty class Rectangle that defines a rectangle without importing any module.
