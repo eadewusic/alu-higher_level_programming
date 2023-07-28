17bd653df147d37887f60618a046c725032cffe6
a0dfc11ba139 <e.adewusi@alustudent.com>
2023-07-28 00:14:59 -0700

    empty class square defining a square

---
+++ python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This is a class module
+that creates a class called Square."""
+
+
+class Square:
+    """This is the class Square
+    that has a private instance method
+    private instance method are created
+    with __attribute(variable)name
+
+    """
+    def __init__(self, size):
+        """
+        this is a built-in special python
+        function that help instantiate object/
+        instance of class variables it is also
+        called an initialization method
+        """
+        self.__size = size
