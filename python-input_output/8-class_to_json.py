18dea1b59df90c0e8138f54e42226801342ecd9c
a0dfc11ba139 <e.adewusi@alustudent.com>
2023-07-28 15:55:36 -0700

    8. Class to JSON

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''Defines function class_to_json
+'''
+
+
+def class_to_json(obj):
+    '''Writes the dictionary description for json serialization of an object
+    '''
+    return (obj.__dict__)`
