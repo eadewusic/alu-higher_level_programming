db72c4585a078d4ccc2a7f1527141315cde1d0d8
a0dfc11ba139 <e.adewusi@alustudent.com>
2023-07-29 02:28:21 -0700

    30. #pythonic

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
     magic_string.count = getattr(magic_string, 'count', 0) + 1
-    return 'BestSchool' + (', BestSchool' * (magic_string.count - 1))
+    return 'Holberton' + (', Holberton' * (magic_string.count - 1))
