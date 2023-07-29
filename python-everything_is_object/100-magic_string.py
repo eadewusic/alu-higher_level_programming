fe9a9fc3e468cff9d37411e0e6112bf6a65ec7f0
a0dfc11ba139 <e.adewusi@alustudent.com>
2023-07-29 02:24:49 -0700

    30. #pythonic

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,14 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    """
-    Returns a string "BestSchool" concatenated with itself n times, where n is the number of times
-    the function has been called.
-
-    Returns:
-        str: A string containing "BestSchool" repeated n times separated by commas.
-    """
-    if not hasattr(magic_string, "count"):
-        magic_string.count = 1
-    else:
-        magic_string.count += 1
-    return "BestSchool" + (", BestSchool" * (magic_string.count - 1))
+    magic_string.count = getattr(magic_string, 'count', 0) + 1
+    return 'BestSchool' + (', BestSchool' * (magic_string.count - 1))
