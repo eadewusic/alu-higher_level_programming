526e5e198455e672373acf98ddf0e6a30a0d99f9
EUNICE ADEWUSI (CLIMIRADI) <e.adewusi@alustudent.com>
2023-08-05 10:10:43 +0300

    Update 6-states.sql

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -9,6 +9,5 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- Step 3: Create the table "states" if it does not exist
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
     id INT AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY,
-    name VARCHAR(256) NOT NULL
-    PRIMARY KEY (id)
+    name VARCHAR(256) NOT NULL,
 );
