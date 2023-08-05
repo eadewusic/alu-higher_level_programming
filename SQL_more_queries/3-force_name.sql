ff47becf0a48a50e8e505607ae7bcf60bebf2a4f
a0dfc11ba139 <e.adewusi@alustudent.com>
2023-08-04 23:48:50 -0700

    Task done

---
+++ SQL_more_queries/3-force_name.sql
@@ -0,0 +1,11 @@
+-- Task: Create the table "force_name" in the specified database
+
+-- Use the specified database (provided as an argument to the mysql command)
+-- USE `#DB_NAME#`;
+
+-- Create the table "force_name" if it does not exist
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
+);
