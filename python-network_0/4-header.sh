f1db07245160dcc29714af74e4ea8417cb46d7ca
EUNICE ADEWUSI (CLIMIRADI) <e.adewusi@alustudent.com>
2023-08-05 13:14:56 +0300

    Bug fixed: 4-header.sh

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes a URL as an argument, sends a GET request to the URL with a custom header, and displays the body of the response.
-curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
+curl -s -i "$1" | grep -E "HTTP" | cut --delimiter=" " -f 3 | tr -d '\r\n'
