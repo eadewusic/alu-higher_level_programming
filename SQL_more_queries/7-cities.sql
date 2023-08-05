2e9fbce65fc301203a59c2746b4aa35c1c57d577
a0dfc11ba139 <e.adewusi@alustudent.com>
2023-08-05 00:19:33 -0700

    Task done

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,14 @@
+-- Task: Create the database "hbtn_0d_usa" and the table "states"
+
+-- Step 1: Create the database "hbtn_0d_usa" if it does not exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Step 2: Use the database "hbtn_0d_usa"
+-- USE hbtn_0d_usa;
+
+-- Step 3: Create the table "states" if it does not exist
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
+);
