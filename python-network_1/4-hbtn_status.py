9ee15087c1deec7cbcccceb5ff5857ddce61572b
EUNICE ADEWUSI (CLIMIRADI) <e.adewusi@alustudent.com>
2023-08-05 14:06:47 +0300

    PEP8 Validation resolved

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -9,7 +9,6 @@ import requests
 
 if __name__ == "__main__":
     r = requests.get("https://intranet.hbtn.io/status")
-    
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))
