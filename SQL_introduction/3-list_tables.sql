0a46f326914a7b61c90441520fe25214c9ff708f
a0dfc11ba139 <e.adewusi@alustudent.com>
2023-08-04 21:30:04 -0700

    script that lists all the tables of a database in your MySQL server

---
+++ SQL_introduction/3-list_tables.sql
@@ -0,0 +1,9 @@
+-- Task: List all tables in a specific database
+-- in your MYSQL server
+
+-- Use the specified database
+-- (provided as an argument to the mysql command)
+USE `#DB_NAME#`;
+
+-- Query to list all tables in the specified database
+SHOW TABLES;
