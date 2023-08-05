61a0784e6c62c6c87f937bef1da2449021c66458
a0dfc11ba139 <e.adewusi@alustudent.com>
2023-08-04 22:06:48 -0700

    Checks corrected

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,7 +1,7 @@
 -- Task: Create the database hbtn_0c_0 if it does not exist
 
 -- Check if the database already exists
-USE hbtn_0c_0;
+-- USE hbtn_0c_0;
 
 -- If the USE statement above does not fail, it means the database exists, so exit without doing anything
 -- If the database does not exist, an error will occur, and we can then proceed to create it
