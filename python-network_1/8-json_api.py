a7f8d16ea187ebe9aa86d2c66412b4e885904a6b
EUNICE ADEWUSI (CLIMIRADI) <e.adewusi@alustudent.com>
2023-08-05 14:19:32 +0300

    PEP8 Validation resolved

--- python-network_1/8-json_api.py
+++ python-network_1/8-json_api.py
@@ -21,7 +21,6 @@ if __name__ == "__main__":
     try:
         response = requests.post(url, data=data)
         json_response = response.json()
-
         if isinstance(json_response, dict) and 'id' in json_response and 'name' in json_response:
             print("[{}] {}".format(json_response['id'], json_response['name']))
         else:
