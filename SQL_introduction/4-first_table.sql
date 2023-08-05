e1396e0af19a837cf8bb1c5a48d96af2a007d26b
a0dfc11ba139 <e.adewusi@alustudent.com>
2023-08-04 22:15:14 -0700

    script that lists all the tables of a database in your MySQL server

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,7 @@
+-- Task: Create a table called "first_table"
+-- in the current database called mysql using
+-- the DBMS called MYSQL SERVER
+CREATE TABLE IF NOT EXISTS first_table(
+	id INT,
+	name VARCHAR(256)
+);
