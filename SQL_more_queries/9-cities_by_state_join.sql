39154a3074363f1c5018246ff0d0787c05f4cf27
EUNICE ADEWUSI (CLIMIRADI) <e.adewusi@alustudent.com>
2023-08-05 10:35:53 +0300

    Bug fixed: 9-cities_by_state_join.sql

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -4,7 +4,7 @@
 -- USE `#DB_NAME#`;
 
 -- List all cities and their corresponding state names
-SELECT cities.id, cities.name AS city_name, states.name
+SELECT cities.id, cities.name, states.name
 FROM cities
 JOIN states ON cities.state_id = states.id
 ORDER BY cities.id;
