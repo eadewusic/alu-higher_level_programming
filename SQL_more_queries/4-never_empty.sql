35385faea3d53bb3dd4e80da37860a03bee89f09
EUNICE ADEWUSI (CLIMIRADI) <e.adewusi@alustudent.com>
2023-08-05 09:56:31 +0300

    Update 4-never_empty.sql

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -5,6 +5,6 @@ USE `#DB_NAME#`;
 
 -- Create the table "id_not_null" if it does not exist
 CREATE TABLE IF NOT EXISTS id_not_null (
-    id INT NOT NULL DEFAULT 1,
+    id INT DEFAULT 1,
     name VARCHAR(256)
 );
