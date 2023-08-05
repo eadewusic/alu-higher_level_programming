f2dce4ca1ac725ee2749aa3671b96f3844071c73
EUNICE ADEWUSI (CLIMIRADI) <e.adewusi@alustudent.com>
2023-08-05 09:23:04 +0300

    Bugs fixed: 1-create_user.sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,10 +1,11 @@
--- Task: Create MySQL server user "user_0d_1" with all privileges
+-- Task: Create the MySQL server user "user_0d_1" with privileges and set password
 
--- Create the user "user_0d_1" with the specified password (if not exists)
+-- Step 1: Create the user "user_0d_1" if it does not exist
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
--- Grant all privileges to the user "user_0d_1" on all databases
+-- Step 2: Grant all privileges to the user "user_0d_1" on all databases
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 
--- Flush privileges to apply the changes
-FLUSH PRIVILEGES;
+-- Step 3: Revoke specific privileges from the user "user_0d_1"
+-- (Optional: Only if certain privileges need to be revoked)
+REVOKE AUDIT_ABORT_EXEMPT, FIREWALL_EXEMPT, AUTHENTICATION_POLICY_ADMIN, GROUP_REPLICATION_STREAM, PASSWORDLESS_USER_ADMIN, SENSITIVE_VARIABLES_OBSERVER, TELEMETRY_LOG_ADMIN ON *.* FROM 'user_0d_1'@'localhost';
