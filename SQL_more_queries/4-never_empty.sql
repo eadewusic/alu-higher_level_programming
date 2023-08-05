fd35a94c3a4365ae98a45ba61320ab84662237e3
EUNICE ADEWUSI (CLIMIRADI) <e.adewusi@alustudent.com>
2023-08-05 09:58:05 +0300

    Update 4-never_empty.sql

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,9 +1,10 @@
 -- Task: Create the table "id_not_null" in the specified database
+
 -- Use the specified database (provided as an argument to the mysql command)
-USE `#DB_NAME#`;
+-- USE `#DB_NAME#`;
 
 -- Create the table "id_not_null" if it does not exist
 CREATE TABLE IF NOT EXISTS id_not_null (
-    id INT DEFAULT 1,
+    id INT NOT NULL DEFAULT 1,
     name VARCHAR(256)
 );
