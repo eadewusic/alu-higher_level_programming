ecb4141312292b99c8d3dfe6bcfe8c18b11c0e6e
EUNICE ADEWUSI (CLIMIRADI) <e.adewusi@alustudent.com>
2023-08-05 08:16:51 +0300

    script that creates a table called first_table in the current database in your MySQL server

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,6 +1,6 @@
 -- Task: Create a table called "first_table"
--- in the current database called mysql using
--- the DBMS called MYSQL SERVER
+-- in the current database called mysql using the
+-- DBMS called MYSQL SERVER
 CREATE TABLE IF NOT EXISTS first_table(
 	id INT,
 	name VARCHAR(256)
