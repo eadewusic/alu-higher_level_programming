28909ea6e95e98c1418776d09bee2e5563c4d84a
a0dfc11ba139 <e.adewusi@alustudent.com>
2023-08-16 07:21:32 -0700

    Task done

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This module defines a function that divides all elements of a matrix
+Prototype: def matrix_divided(matrix, div):
+You are not allowed to import any module
+"""
+
+
+def matrix_divided(matrix, div):
+    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+    if not all(len(row) == len(matrix[0]) for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
+
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    new_matrix = [[round(element / div, 2) for element in row] for row in matrix]
+    return new_matrix
