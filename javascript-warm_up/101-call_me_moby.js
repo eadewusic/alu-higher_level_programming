a3fb32870d2f7b88d6ede2180afa62d88fca0ec9
a0dfc11ba139 <e.adewusi@alustudent.com>
2023-09-30 11:49:32 -0700

    15. Call me Moby

---
+++ javascript-warm_up/101-call_me_moby.js
@@ -0,0 +1,13 @@
+// This is the file containing the function
+
+#!/usr/bin/node
+
+function callMeMoby(x, theFunction) {
+  for (let i = 0; i < x; i++) {
+    theFunction();
+  }
+}
+
+module.exports = {
+  callMeMoby: callMeMoby
+};
