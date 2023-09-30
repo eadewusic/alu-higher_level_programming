44d707af4184d3c8b4f0952dd797d8e163f95752
a0dfc11ba139 <e.adewusi@alustudent.com>
2023-09-30 12:15:42 -0700

    changes made

--- javascript-warm_up/100-let_me_const.js
+++ javascript-warm_up/100-let_me_const.js
@@ -2,4 +2,4 @@
 
 // #!/usr/bin/node
 
-let myVar = 333;
+const myVar = 333;
--- javascript-warm_up/101-call_me_moby.js
+++ javascript-warm_up/101-call_me_moby.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-function callMeMoby(x, theFunction) {
+function callMeMoby (x, theFunction) {
   for (let i = 0; i < x; i++) {
     theFunction();
   }
 }
 
 module.exports = {
-  callMeMoby: callMeMoby
+  callMeMoby
 };
