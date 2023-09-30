68e6c9cb679f41d3a5c9ef78d783c490e8b34bf1
a0dfc11ba139 <e.adewusi@alustudent.com>
2023-09-30 16:51:03 -0700

    semistandard validation fixed

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
   }
 
-  print() {
+  print () {
     if (this.width && this.height) {
       for (let i = 0; i < this.height; i++) {
         console.log('X'.repeat(this.width));
@@ -16,7 +16,7 @@ class Rectangle {
     }
   }
 
-  rotate() {
+  rotate () {
     if (this.width && this.height) {
       const temp = this.width;
       this.width = this.height;
@@ -24,7 +24,7 @@ class Rectangle {
     }
   }
 
-  double() {
+  double () {
     if (this.width && this.height) {
       this.width *= 2;
       this.height *= 2;
