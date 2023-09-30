203b83c0b405405c30160e6e4d93077b033322cb
a0dfc11ba139 <e.adewusi@alustudent.com>
2023-09-30 10:41:09 -0700

    10. Factorial

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+function factorial(n) {
+  if (isNaN(n)) {
+    return 1;
+  }
+
+  n = parseInt(n, 10);
+
+  if (n < 0) {
+    return Infinity;
+  }
+
+  if (n === 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+
+const arg = process.argv[2];
+const result = factorial(arg);
+console.log(result);
