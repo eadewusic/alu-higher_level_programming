4cd5a96dfaba7d3aa1b942552c6acedf6f08edae
a0dfc11ba139 <e.adewusi@alustudent.com>
2023-09-30 11:19:32 -0700

    semistandard validation fixed

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
 // Define the function
-function add(a, b) {
+function add (a, b) {
   return a + b;
 }
 
 // Export the function so it can be used from outside
 module.exports = {
-  add: add
+  add
 };
