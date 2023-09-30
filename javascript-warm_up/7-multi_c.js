4f8dd650bfb724e6ce44c450e9c0d6e73b05c3c9
a0dfc11ba139 <e.adewusi@alustudent.com>
2023-09-30 10:19:37 -0700

    7. I love C

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const arg = process.argv[2];
+
+if (isNaN(arg)) {
+  console.log('Missing number of occurrences');
+}
+else {
+  const x = parseInt(arg, 10);
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
+}
