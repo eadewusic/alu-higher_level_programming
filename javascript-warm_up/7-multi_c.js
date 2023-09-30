8566b250032d38283a36b07275af96ae6074c6d7
a0dfc11ba139 <e.adewusi@alustudent.com>
2023-09-30 10:23:46 -0700

    semistandard validation fixed

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -4,8 +4,7 @@ const arg = process.argv[2];
 
 if (isNaN(arg)) {
   console.log('Missing number of occurrences');
-}
-else {
+} else {
   const x = parseInt(arg, 10);
   for (let i = 0; i < x; i++) {
     console.log('C is fun');
