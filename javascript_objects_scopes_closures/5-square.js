1244452e0bf370455241fb9739e0c6dbb8387bb0
a0dfc11ba139 <e.adewusi@alustudent.com>
2023-09-30 16:58:10 -0700

    semistandard validation fixed

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -2,7 +2,7 @@
 const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
-  constructor(size) {
+  constructor (size) {
     super(size, size); // Call the constructor of the Rectangle class with size for both width and height
   }
 }
