527c99eece248e110718afaa705726c63fba9870
a0dfc11ba139 <e.adewusi@alustudent.com>
2023-09-30 09:03:30 -0700

    semistandard validation fixed

--- javascript-warm_up/0-javascript_is_amazing.js
+++ javascript-warm_up/0-javascript_is_amazing.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 // Create a constant variable called myVar with the value "JavaScript is amazing"
-const myVar = "JavaScript is amazing";
+const myVar = 'JavaScript is amazing';
 
 // Use console.log(...) to print the output
 console.log(myVar);
