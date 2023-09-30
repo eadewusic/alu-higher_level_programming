ae9466630d393c9b1c1e6f5c79a3df30ec1d661c
a0dfc11ba139 <e.adewusi@alustudent.com>
2023-09-30 10:32:34 -0700

    9. Add

---
+++ javascript-warm_up/9-add.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+function add(a, b) {
+  return a + b;
+}
+
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+
+if (isNaN(arg1) || isNaN(arg2)) {
+  console.log('NaN');
+} else {
+  const num1 = parseInt(arg1, 10);
+  const num2 = parseInt(arg2, 10);
+  const result = add(num1, num2);
+  console.log(result);
+}
