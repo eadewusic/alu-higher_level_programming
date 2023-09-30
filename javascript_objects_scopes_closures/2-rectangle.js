46faae8c76fd94798442f47c88c2fd2bbe389041
EUNICE ADEWUSI (CLIMIRADI) <e.adewusi@alustudent.com>
2023-10-01 01:23:48 +0200

    Update 2-rectangle.js

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 class Rectangle {
-  constructor(w, h) {
-    if (w <= 0 || h <= 0) {
-      // If w or h is not a positive integer or is 0, create an empty object.
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || typeof w === 'undefined' || typeof h === 'undefined') {
+      // If either w or h is not a positive integer or is 0, or if either is undefined, create an empty object.
       return {};
     } else {
       this.width = w;
