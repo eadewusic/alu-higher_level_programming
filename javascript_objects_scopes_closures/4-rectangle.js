ca0f394863af47ef86157e8df849f834e7a2b1e1
a0dfc11ba139 <e.adewusi@alustudent.com>
2023-09-30 16:46:56 -0700

    4. Rectangle #4

---
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,35 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor(w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print() {
+    if (this.width && this.height) {
+      for (let i = 0; i < this.height; i++) {
+        console.log('X'.repeat(this.width));
+      }
+    }
+  }
+
+  rotate() {
+    if (this.width && this.height) {
+      const temp = this.width;
+      this.width = this.height;
+      this.height = temp;
+    }
+  }
+
+  double() {
+    if (this.width && this.height) {
+      this.width *= 2;
+      this.height *= 2;
+    }
+  }
+}
+
+module.exports = Rectangle;
