23f28131a9140fe2d7543e37eaa1ec221c13c6ca
a0dfc11ba139 <e.adewusi@alustudent.com>
2023-09-30 17:33:03 -0700

    12. Sorted occurences

---
+++ javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const dict = require('./101-data').dict;
+
+// Create a new dictionary to store user ids by occurrence
+const userByOccurrence = {};
+
+// Iterate through the original dictionary
+for (let userId in dict) {
+  const occurrence = dict[userId];
+
+  // Check if the occurrence exists in the new dictionary
+  if (userByOccurrence[occurrence] === undefined) {
+    userByOccurrence[occurrence] = [];
+  }
+
+  // Add the user id to the corresponding occurrence key
+  userByOccurrence[occurrence].push(userId);
+}
+
+console.log(userByOccurrence);
