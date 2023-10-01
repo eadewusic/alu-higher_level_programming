dcf30cb3e95ac6159a1e60441184b3a73eb95feb
a0dfc11ba139 <e.adewusi@alustudent.com>
2023-09-30 17:07:59 -0700

    7. Occurrences

---
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+
+  for (let element of list) {
+    if (element === searchElement) {
+      count++;
+    }
+  }
+
+  return count;
+};
