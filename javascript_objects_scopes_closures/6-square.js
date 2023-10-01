38a2ea26c0ede8f2c51439e9732239e724e3ca63
a0dfc11ba139 <e.adewusi@alustudent.com>
2023-09-30 17:01:46 -0700

    6. Square #1

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const ParentSquare = require('./5-square'); // Import the parent Square class
+
+class Square extends ParentSquare {
+  charPrint(c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
