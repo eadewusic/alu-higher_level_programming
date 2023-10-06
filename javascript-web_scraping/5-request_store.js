1a36f524bc41d960b0d856cb8406cd76ef195cab
EUNICE ADEWUSI (CLIMIRADI) <e.adewusi@alustudent.com>
2023-10-07 01:17:46 +0200

    Update 5-request_store.js

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -4,18 +4,20 @@
 const { argv } = require('process');
 const request = require('request'); // Import the 'request' module to make HTTP requests
 
+// Make an HTTP GET request to the URL provided as the first command-line argument (argv[2])
 request(argv[2], (error, response, body) => {
   if (error) {
-    // If there's an error during the request, log the error
+    // If there's an error during the request, log the error to the console
     console.log(error);
-    return;
+    return; // Exit the script if an error occurs
   }
+  
   const fs = require('fs'); // Import the 'fs' module to work with the file system
 
-  // Write the response body (webpage content) to the specified file
+  // Write the response body (webpage content) to the file specified as the second command-line argument (argv[3])
   fs.writeFile(argv[3], body, (err) => {
     if (err) {
-      // If there's an error while writing to the file, log the error
+      // If there's an error while writing to the file, log the error to the console
       console.log(err);
     }
   });
