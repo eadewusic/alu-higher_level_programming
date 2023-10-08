4c762169fd89bb90bb82decb6251111f5791597e
EUNICE ADEWUSI (CLIMIRADI) <e.adewusi@alustudent.com>
2023-10-08 20:37:36 +0200

    Update 0-script.js

--- javascript-web_query/0-script.js
+++ javascript-web_query/0-script.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
 // This JavaScript script updates the text color of the <header> element to red.
 
-// Wait for the DOM to fully load before executing the script
-document.addEventListener('DOMContentLoaded', function () {
-  // Select the <header> element using document.querySelector
-  const header = document.querySelector('header');
-  
-  // Update the text color of the <header> element to red (#FF0000)
-  header.style.color = '#FF0000';
-});
+// Use document.querySelector to select the <header> tag
+const header = document.querySelector('header');
+
+// Update the text color of the <header> element to red (#FF0000)
+header.style.color = '#FF0000';
