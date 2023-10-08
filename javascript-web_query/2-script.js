3b839946b000c82a5289a3d3828ee786709f2f14
Eunice Adewusi <e.adewusi@alustudent.com>
2023-10-08 21:07:07 +0200

    2. Click and turn red

--- javascript-web_query/2-script.js
+++ javascript-web_query/2-script.js
@@ -2,12 +2,12 @@
 // This JavaScript script updates the text color of the <header> element to red when the user clicks on the tag DIV#red_header using jQuery.
 
 $(document).ready(function() {
-    // Use the jQuery API to select the <div> element with id "red_header"
-    const redHeader = $('#red_header');
-  
-    // Define a click event handler
-    redHeader.click(function() {
-      // Update the text color of the <header> element to red (#FF0000) using the .css() method
-      $('header').css('color', '#FF0000');
-    });
-  });  
+  // Use the jQuery API to select the <div> element with id "red_header"
+  const redHeader = $('#red_header');
+
+  // Define a click event handler
+  redHeader.click(function() {
+    // Update the text color of the <header> element to red (#FF0000) using the .css() method
+    $('header').css('color', '#FF0000');
+  });
+});
